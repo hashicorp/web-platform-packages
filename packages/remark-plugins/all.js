387264bd3037ed53c83a042b415b20cb4620cead
Kevin Wang <kwangsan@gmail.com>
2021-11-19 15:30:50 -0500

    feat(remark-plugins): `jump-to-section`

--- packages/remark-plugins/all.js
+++ packages/remark-plugins/all.js
@@ -1,4 +1,5 @@
 import anchorLinks from './plugins/anchor-links/index.js'
+import jumpToSection from './plugins/jump-to-section/index.js'
 import paragraphCustomAlerts from './plugins/paragraph-custom-alerts/index.js'
 import typography from './plugins/typography/index.js'
 import includeMarkdown from './plugins/include-markdown/index.js'
@@ -6,12 +7,14 @@ import includeMarkdown from './plugins/include-markdown/index.js'
 // for easy use of everything at the same time
 export default function allPlugins({
   anchorLinks: anchorLinksOptions,
+
   typography: typographyOptions,
   includeMarkdown: includeMarkdownOptions,
 } = {}) {
   return [
     [includeMarkdown, includeMarkdownOptions],
     [anchorLinks, anchorLinksOptions],
+    jumpToSection,
     paragraphCustomAlerts,
     [typography, typographyOptions],
   ]
