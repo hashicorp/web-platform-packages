3d6ceff1cd13023c5cda944fff0c6f350b7d5087
Kevin Wang <kwangsan@gmail.com>
2021-11-19 21:26:21 -0500

    Update packages/remark-plugins/plugins/jump-to-section/index.js

--- packages/remark-plugins/plugins/jump-to-section/index.js
+++ packages/remark-plugins/plugins/jump-to-section/index.js
@@ -8,19 +8,10 @@ export default function jumpToSection() {
       if (is(node, 'heading') && index === 0) {
         return [
           node,
-          u('mdxJsxFlowElement', {
-            name: 'JumpToSection',
-            attributes: [
-              u('mdxJsxAttribute', {
-                name: 'headings',
-                value: u(
-                  'mdxJsxAttributeValueExpression',
-                  { data: { estree: undefined } },
-                  'headings'
-                ),
-              }),
-            ],
-          }),
+          u("jsx", {
+            value: "<JumpToSection headings={headings}/>",
+          })
+
         ]
       }
 
