f3b0cf51e461c3f1700f139f89fb0c48c9c6c20e
Bryce Kalow <bkalow@hashicorp.com>
2021-11-22 10:48:04 -0600

    Update source to esm and update tests

--- packages/code-highlighting/fixtures/_read-file.js
+++ packages/code-highlighting/fixtures/_read-file.js
@@ -1,8 +1,10 @@
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-function readFile(file) {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default function readFile(file) {
   return fs.readFileSync(path.join(__dirname, `./${file}`), 'utf-8')
 }
-
-module.exports = readFile
