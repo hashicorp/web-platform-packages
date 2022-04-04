e3cf66dff83c0b32ecb4a003c5a2a3feac35abd6
Bryce Kalow <bkalow@hashicorp.com>
2022-04-04 16:31:03 -0500

    refactor and also add the local-preview script

--- packages/tools/scripts/add-deploy-preview-script/index.ts
+++ packages/tools/scripts/add-deploy-preview-script/index.ts
@@ -1,15 +1,27 @@
 import path from 'path'
 import fs from 'fs'
 
-const templatePath = path.join(__dirname, 'script.template.txt')
-const destinationPath = path.join(process.cwd(), 'scripts', 'website-build.sh')
+const files = [
+  {
+    template: path.join(__dirname, 'deploy-preview-script.template.txt'),
+    destination: path.join(process.cwd(), 'scripts', 'website-build.sh'),
+  },
+  {
+    template: path.join(__dirname, 'local-preview-script.template.txt'),
+    destination: path.join(process.cwd(), 'scripts', 'website-start.sh'),
+  },
+]
 
 export default async function main(product: string) {
-  const template = String(await fs.promises.readFile(templatePath))
-  const script = template.replace('{PRODUCT}', product)
+  for (const file of files) {
+    const { template: templatePath, destination: destinationPath } = file
 
-  console.log('writing script to:', destinationPath)
-  await fs.promises.writeFile(destinationPath, script, { encoding: 'utf-8' })
-  // ensure the script file is executable after write
-  await fs.promises.chmod(destinationPath, '755')
+    const template = String(await fs.promises.readFile(templatePath))
+    const script = template.replace('{PRODUCT}', product)
+
+    console.log('writing script to:', destinationPath)
+    await fs.promises.writeFile(destinationPath, script, { encoding: 'utf-8' })
+    // ensure the script file is executable after write
+    await fs.promises.chmod(destinationPath, '755')
+  }
 }
