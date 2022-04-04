02914d7128cca1d6e5dc49fbe55759b1d3040452
Bryce Kalow <bkalow@hashicorp.com>
2022-04-04 16:52:00 -0500

    update package.json

--- packages/tools/scripts/add-deploy-preview-script/index.ts
+++ packages/tools/scripts/add-deploy-preview-script/index.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import fs from 'fs'
 
+const pkgJsonPath = path.join(process.cwd(), 'package.json')
+
 const files = [
   {
     template: path.join(__dirname, 'deploy-preview-script.template.txt'),
@@ -24,4 +26,14 @@ export default async function main(product: string) {
     // ensure the script file is executable after write
     await fs.promises.chmod(destinationPath, '755')
   }
+
+  // Update the package.json scripts
+  const pkgJson = JSON.parse(String(await fs.promises.readFile(pkgJsonPath)))
+
+  pkgJson.scripts.start = './scripts/website-start.sh'
+  pkgJson.scripts.build = './scripts/website-build.sh'
+
+  await fs.promises.writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, 2), {
+    encoding: 'utf-8',
+  })
 }
