f8a6d886fa9ca699786fdd91d64291cf1ecf8b69
Bryce Kalow <bkalow@hashicorp.com>
2022-04-14 15:10:49 -0500

    Update packages/packer-plugins/constants.ts
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

--- packages/packer-plugins/constants.ts
+++ packages/packer-plugins/constants.ts
@@ -1,3 +1,7 @@
+/**
+ * A list of Packer plugin component types. This maps to directory names within `./docs`:
+ * - @see https://github.com/hashicorp/packer-plugin-scaffolding/tree/main/docs
+ */
 export const COMPONENT_TYPES = [
   'builders',
   'datasources',
