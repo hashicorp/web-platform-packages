c6a5d0518f3fe152adf6759ecb71a6aeeb753e36
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-07-13 14:11:54 -0400

    remove test stubs

--- packages/analytics/utm-params-capture/index.test.js
+++
@@ -1,9 +0,0 @@
-import { initializeUTMParamsCapture, getUTMParamsCaptureState } from './'
-
-describe('initializeUTMParamsCapture', () => {
-  it.todo('should set allowed list of UTM params as cookies', () => {})
-})
-
-describe('getUTMParamsCaptureState', () => {
-  it.todo('should return an object of UTM params from cookies', () => {})
-})
