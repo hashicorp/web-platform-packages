1e51cec2c65480dd1bb587bc8f50c049f7c6e33a
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-07-14 11:08:17 -0400

    reverts testEnv change

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   verbose: true,
   globals: {
     'ts-jest': {
--- packages/analytics/get-product-intent-from-url/index.test.js
+++ packages/analytics/get-product-intent-from-url/index.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 const { getProductIntentFromURL } = require('./')
 
 describe('getProductIntentFromURL', () => {
