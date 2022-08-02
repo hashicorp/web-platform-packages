b09c711ae3bc2f4118f8ae167afa27187608245f
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-08-02 11:10:59 -0400

    init addCloudLinkHandler

---
+++ packages/analytics/add-cloud-link-handler/index.ts
@@ -0,0 +1,52 @@
+import { getProductIntentFromURL } from '../get-product-intent-from-url'
+import { getUTMParamsCaptureState } from '../utm-params-capture'
+
+const destinations: string[] = [
+  'portal.cloud.hashicorp.com',
+  'app.terraform.io',
+]
+const containsDestination = (str: string): boolean =>
+  destinations.some(function (v) {
+    return str.indexOf(v) >= 0
+  })
+
+export function addCloudLinkHandler() {
+  if (typeof window === 'undefined') return
+
+  window.addEventListener('click', (event) => {
+    /**
+     * Current <Button /> component implementation wraps the
+     * inner button elements within a <span /> which requires us
+     * to traverse up the dom to the closest anchor tag.
+     * See {@link https://developer.mozilla.org/en-US/docs/Web/API/Event/composedPath}
+     */
+    const composedPath = event.composedPath()
+
+    composedPath.forEach((path: EventTarget) => {
+      if (
+        path &&
+        (path as HTMLElement).nodeName === 'A' &&
+        containsDestination((path as HTMLAnchorElement).href)
+      ) {
+        event.preventDefault()
+        const productIntent = getProductIntentFromURL()
+        const searchParams = {
+          ...getUTMParamsCaptureState(),
+          ...(productIntent && {
+            product: productIntent,
+          }),
+        }
+        try {
+          const url = new URL((path as HTMLAnchorElement).href)
+          location.href = `${url.origin}${
+            Object.keys(searchParams).length > 0
+              ? `?${new URLSearchParams(searchParams).toString()}`
+              : ''
+          }`
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    })
+  })
+}
