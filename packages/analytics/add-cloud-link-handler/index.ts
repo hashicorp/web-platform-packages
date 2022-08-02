76f2fa4ed39e3f013cd762595cc4a52cd09caebb
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-08-02 15:14:42 -0400

    refactor to use closest

--- packages/analytics/add-cloud-link-handler/index.ts
+++ packages/analytics/add-cloud-link-handler/index.ts
@@ -14,39 +14,26 @@ export function addCloudLinkHandler() {
   if (typeof window === 'undefined') return
 
   window.addEventListener('click', (event) => {
-    /**
-     * Current <Button /> component implementation wraps the
-     * inner button elements within a <span /> which requires us
-     * to traverse up the dom to the closest anchor tag.
-     * See {@link https://developer.mozilla.org/en-US/docs/Web/API/Event/composedPath}
-     */
-    const composedPath = event.composedPath()
-
-    composedPath.forEach((path: EventTarget) => {
-      if (
-        path &&
-        (path as HTMLElement).nodeName === 'A' &&
-        containsDestination((path as HTMLAnchorElement).href)
-      ) {
-        event.preventDefault()
-        const productIntent = getProductIntentFromURL()
-        const searchParams = {
-          ...getUTMParamsCaptureState(),
-          ...(productIntent && {
-            product: productIntent,
-          }),
-        }
-        try {
-          const url = new URL((path as HTMLAnchorElement).href)
-          location.href = `${url.origin}${
-            Object.keys(searchParams).length > 0
-              ? `?${new URLSearchParams(searchParams).toString()}`
-              : ''
-          }`
-        } catch (error) {
-          console.error(error)
-        }
+    const linkElement = (event.target as HTMLElement).closest('a')
+    if (linkElement && containsDestination(linkElement.href)) {
+      event.preventDefault()
+      const productIntent = getProductIntentFromURL()
+      const searchParams = {
+        ...getUTMParamsCaptureState(),
+        ...(productIntent && {
+          product: productIntent,
+        }),
       }
-    })
+      try {
+        const url = new URL(linkElement.href)
+        location.href = `${url.origin}${
+          Object.keys(searchParams).length > 0
+            ? `?${new URLSearchParams(searchParams).toString()}`
+            : ''
+        }`
+      } catch (error) {
+        console.error(error)
+      }
+    }
   })
 }
