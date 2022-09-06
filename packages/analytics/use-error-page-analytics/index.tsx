e780333372b4099113ffe59d44ae3f9658d954a6
Nels Andereck <nels.andereck@hashicorp.com>
2022-09-06 11:04:40 -0700

    rm cruft condition and update comments

--- packages/analytics/use-error-page-analytics/index.tsx
+++ packages/analytics/use-error-page-analytics/index.tsx
@@ -7,18 +7,16 @@ import { useEffect } from 'react'
  * the specified error at the current window.location.href.
  *
  * Relies on window.analytics.track() being a valid function
- * which can be called as window.analytics.track(href, { category, label }).
+ * which can be called as window.analytics.track('Error Page Loaded', { http_status_code, label, referrer }).
  */
 export default function useErrorPageAnalytics(
-  /** The type of error. Used to send a "{statusCode} Response"
-   * category value to window.analytics.track, if that's possible. */
+  /** The HTTP status code to send with the track event under `http_status_code` */
   statusCode: number
 ): void {
   useEffect(() => {
     if (
       typeof window !== 'undefined' &&
       typeof window?.analytics?.track === 'function' &&
-      typeof window?.document?.referrer === 'string' &&
       typeof window?.location?.href === 'string'
     )
       window.analytics.track('Error Page Loaded', {
