97c779a9106464c28229aa141e966744cea91e3e
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-10-21 13:22:48 -0400

    add isValidUrl check

--- packages/analytics/add-anonymous-id-handler/index.ts
+++ packages/analytics/add-anonymous-id-handler/index.ts
@@ -18,36 +18,36 @@ export function addAnonymousIdHandler() {
 
   window.addEventListener('click', (event) => {
     const linkElement = (event.target as HTMLElement).closest('a')
-    if (linkElement && containsDestination(linkElement.href)) {
+    if (
+      linkElement &&
+      containsDestination(linkElement.href) &&
+      isValidUrl(linkElement.href)
+    ) {
       event.preventDefault()
-      try {
-        const url = new URL(linkElement.href)
-        const ajs_uid = safeGetSegmentId()
-        if (ajs_uid) {
-          url.searchParams.append('ajs_uid', ajs_uid)
-        }
-        if (
-          linkElement.getAttribute('target') === '_blank' ||
-          event.ctrlKey ||
-          event.metaKey
-        ) {
-          window.open(url.href, '_blank')
-        } else {
-          location.href = url.href
-        }
-      } catch {
-        if (
-          linkElement.getAttribute('target') === '_blank' ||
-          event.ctrlKey ||
-          event.metaKey
-        ) {
-          window.open(linkElement.href, '_blank')
-        } else {
-          location.href = linkElement.href
-        }
+      const url = new URL(linkElement.href)
+      const ajs_uid = safeGetSegmentId()
+      if (ajs_uid) {
+        url.searchParams.append('ajs_uid', ajs_uid)
+      }
+      if (
+        linkElement.getAttribute('target') === '_blank' ||
+        event.ctrlKey ||
+        event.metaKey
+      ) {
+        window.open(url.href, '_blank')
+      } else {
+        location.href = url.href
       }
     }
   })
+}
+
+function isValidUrl(url: string): boolean {
+  try {
+    return Boolean(new URL(url))
+  } catch (e) {
+    return false
+  }
 }
 
 function safeGetSegmentId(): string | null {
