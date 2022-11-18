542fb50d0f88cb175b89b5086f82de1ea3c4fbe8
Nick Ayres <nickolas.ayres@hashicorp.com>
2022-11-17 18:37:52 -0700

    Add basic content conformance CLI

--- packages/content-conformance/src/cli.ts
+++ packages/content-conformance/src/cli.ts
@@ -1,17 +1,53 @@
 #!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import chalk from 'chalk'
 import { ContentConformanceRunner } from './runner.js'
 
 /**
  * The CLI interface for our content conformance runner. Should remain a pass-through to ContentConformanceRunner if at all possible to keep the CLI and JS API in-sync
  */
-async function main() {
-  const runner = new ContentConformanceRunner()
+yargs(hideBin(process.argv)).command(
+  'content-check',
+  'Content conformance runner',
+  {
+    cwd: {
+      description: 'Current working directory',
+      default: undefined,
+      type: 'string',
+    },
+    config: {
+      description: 'Path to config file',
+      default: undefined,
+      type: 'string',
+    },
+    files: {
+      description: 'Path to config file',
+      default: [],
+      type: 'array',
+    },
+  },
+  async function (argv) {
+    const runner = new ContentConformanceRunner({
+      cwd: argv.cwd,
+      config: argv.config,
+      files: argv.files,
+    })
 
-  await runner.init()
+    await runner
+      .init()
+      .then(() => {
+        console.log(
+          chalk.bold.greenBright(`Running content conformance checks on:`)
+        )
+        argv.files.forEach((file: string) => {
+          console.log(chalk.green(`- ${file}`))
+        })
+      })
+      .catch((error) => {
+        console.log(chalk.redBright(error.stack))
+      })
 
-  console.log(runner.config)
-}
-
-main()
-
-export {}
+    console.log(chalk.cyanBright(`Config: ${runner.config}`))
+  }
+).argv
