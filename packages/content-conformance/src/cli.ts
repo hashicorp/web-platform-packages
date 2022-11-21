a81dd46233332cd6c1707c5b2db8099ca3aaf8f2
Nick Ayres <40183141+nayres@users.noreply.github.com>
2022-11-21 16:16:41 -0700

    Add basic content conformance CLI (#120)
    * Add basic content conformance CLI
    
    * patch bump
    
    * Update packages/content-conformance/package.json
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    
    * Update packages/content-conformance/src/cli.ts
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>
    
    * PR notes clean up
    
    * Reinstall yaml dep, remove unused type and function
    
    * Added conditions to determine what is logged
    
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>

--- packages/content-conformance/src/cli.ts
+++ packages/content-conformance/src/cli.ts
@@ -1,17 +1,65 @@
 #!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import chalk from 'chalk'
 import { ContentConformanceRunner } from './runner.js'
 
 /**
  * The CLI interface for our content conformance runner. Should remain a pass-through to ContentConformanceRunner if at all possible to keep the CLI and JS API in-sync
  */
-async function main() {
-  const runner = new ContentConformanceRunner()
+yargs(hideBin(process.argv)).command(
+  'content-check',
+  'Content conformance runner',
+  {
+    cwd: {
+      description: 'Current working directory',
+      default: undefined,
+      type: 'string',
+    },
+    config: {
+      description: 'Path to config file',
+      default: undefined,
+      type: 'string',
+    },
+    files: {
+      description: 'Path to content files',
+      default: [],
+      type: 'array',
+    },
+  },
+  async function (argv): Promise<void> {
+    const runner = new ContentConformanceRunner({
+      cwd: argv.cwd,
+      config: argv.config,
+      files: argv.files,
+    })
 
-  await runner.init()
+    try {
+      await runner.init()
 
-  console.log(runner.config)
-}
+      console.log(
+        chalk.bold.greenBright(`Running content conformance checks...`)
+      )
 
-main()
+      if (!argv.files.length) return
 
-export {}
+      console.log(
+        chalk.bold.green(
+          `Included ${argv.files.length > 1 ? 'files' : 'file'}:`
+        )
+      )
+
+      argv.files.forEach((file: string) => {
+        console.log(chalk.whiteBright(`- ${file}`))
+      })
+    } catch (error) {
+      let stack = 'Unknown Error'
+      if (error instanceof Error) stack = error?.stack ?? stack
+      else stack = String(error)
+
+      console.log(chalk.redBright(stack))
+    }
+
+    console.log(chalk.cyanBright(`Config: ${runner.config}`))
+  }
+).argv
