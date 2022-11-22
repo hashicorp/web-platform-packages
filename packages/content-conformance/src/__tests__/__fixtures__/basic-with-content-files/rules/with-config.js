1901c99ec506956bac9aa191f017ee317c21d794
Bryce Kalow <bkalow@hashicorp.com>
2022-11-22 15:01:51 -0600

    support passing rule config, making it available via the rule context

---
+++ packages/content-conformance/src/__tests__/__fixtures__/basic-with-content-files/rules/with-config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  type: 'content',
+  id: 'with-config',
+  description: 'Shows how to use rule config off of the context',
+  executor: {
+    async contentFile(file, context) {
+      file.visit(['heading'], (node) => {
+        // @ts-expect-error -- Node is a generic type
+        if (node.depth === 1) {
+          context.report(
+            `Level 1 headings are not allowed, ${context.config.message}`,
+            file,
+            node
+          )
+        }
+      })
+    },
+    dataFile() {},
+  },
+}
