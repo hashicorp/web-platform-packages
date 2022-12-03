22ab7f57286fdd6563523a73c41afe18fc0b2e99
Kevin Wang <kwangsan@gmail.com>
2022-12-02 21:50:56 -0500

    feat: use zod for bespoke validation checks

--- packages/integrations-hcl/index.ts
+++ packages/integrations-hcl/index.ts
@@ -1,4 +1,6 @@
-import fs from 'fs'
+import * as path from 'path'
+import * as fs from 'fs'
+
 import { fetchIntegration, Product } from './lib/api-client'
 
 import HCL from './lib/hcl'
@@ -42,10 +44,11 @@ export default async function LoadFilesystemIntegration(
   const apiIntegration = integrationFetchResult.result
 
   // Parse out & validate the metadata.hcl file
-  const repoRootDirectory = `${config.repo_path}${
-    apiIntegration.subdirectory || ''
-  }`
-  const metadataFilePath = `${repoRootDirectory}/metadata.hcl`
+  const repoRootDirectory = path.join(
+    config.repo_path,
+    apiIntegration.subdirectory
+  )
+  const metadataFilePath = path.join(repoRootDirectory, 'metadata.hcl')
 
   const fileContent = fs.readFileSync(metadataFilePath, 'utf8')
   const hclConfig = new HCL(fileContent, MetadataHCLSchema)
@@ -54,55 +57,15 @@ export default async function LoadFilesystemIntegration(
   }
   const hclIntegration = hclConfig.result.data.integration[0]
 
-  // Calculate the Docs Block
-  const docs: Docs = {
-    process_docs: true,
-    readme_location: './README.md',
-    external_url: null,
-  }
-  if (hclIntegration.docs && hclIntegration.docs[0]) {
-    const hclDocsBlock = hclIntegration.docs[0]
-    if (typeof hclDocsBlock.process_docs !== 'undefined') {
-      docs.process_docs = hclDocsBlock.process_docs
-    }
-    if (typeof hclDocsBlock.readme_location !== 'undefined') {
-      docs.readme_location = hclDocsBlock.readme_location
-    }
-    if (typeof hclDocsBlock.external_url !== 'undefined') {
-      docs.external_url = hclDocsBlock.external_url
-    }
-  }
-
-  // Calculate License Block
-  const license: License = {
-    type: null,
-    url: null,
-  }
-  if (hclIntegration.license && hclIntegration.license[0]) {
-    const licenseBlock = hclIntegration.license[0]
-    license.type = licenseBlock.type || null
-    license.url = licenseBlock.url || null
-  }
-
   // Read the README
   let readmeContent: string | null = null
-  if (docs.process_docs) {
-    const readmeFile = `${config.repo_path}${
-      apiIntegration.subdirectory || ''
-    }/${docs.readme_location}`
-    readmeContent = fs.readFileSync(readmeFile, 'utf8')
-  }
-
-  // Ensure that external_url is provided if process_docs is false
-  if (!docs.process_docs && !docs.external_url) {
-    throw new Error(
-      '`docs.external_url` is required if `docs.process_docs` is set to false.'
+  if (hclIntegration.docs[0].process_docs) {
+    const readmeFile = path.join(
+      config.repo_path,
+      apiIntegration.subdirectory || '',
+      hclIntegration.docs[0].readme_location
     )
-  }
-
-  // Ensure there is at least one component
-  if (hclIntegration.components.length < 1) {
-    throw new Error('At least one component is required.')
+    readmeContent = fs.readFileSync(readmeFile, 'utf8')
   }
 
   // Calculate each Component object
@@ -125,13 +88,10 @@ export default async function LoadFilesystemIntegration(
       readme: readmeContent,
       components: allComponents,
     },
-    flags: hclIntegration.flags ? hclIntegration.flags : [],
-    docs: docs,
-    hide_versions:
-      typeof hclIntegration.hide_versions !== 'undefined'
-        ? hclIntegration.hide_versions
-        : false,
-    license: license,
+    flags: hclIntegration.flags,
+    docs: hclIntegration.docs[0],
+    hide_versions: hclIntegration.hide_versions,
+    license: hclIntegration.license[0],
   }
 }
 
--- packages/integrations-hcl/schemas/metadata.hcl.ts
+++ packages/integrations-hcl/schemas/metadata.hcl.ts
@@ -1,25 +1,52 @@
 import { z } from 'zod'
 
+// https://github.com/hashicorp/integration-template
 const license = z.object({
-  type: z.string().optional(),
-  url: z.string().url().optional(),
+  type: z.string().nullable().default(null),
+  url: z.string().url().nullable().default(null),
 })
 
-const docs = z.object({
-  process_docs: z.boolean().optional(),
-  readme_location: z.string().optional(),
-  external_url: z.string().url().optional(),
-})
+const docs = z.discriminatedUnion('process_docs', [
+  z.object({
+    process_docs: z.literal(false),
+    readme_location: z.string().default('./README.md'),
+    external_url: z
+      .string({
+        required_error:
+          'docs.external_url is required if docs.process_docs is false',
+        invalid_type_error: 'docs.external_url must be a valid URL',
+      })
+      .url('docs.external_url must be a valid URL'),
+  }),
+  z.object({
+    process_docs: z.literal(true),
+    readme_location: z.string().default('./README.md'),
+    external_url: z.string().nullable().default(null),
+  }),
+])
 
 const integration = z.object({
+  // required
   name: z.string(),
   description: z.string(),
   identifier: z.string(),
-  components: z.string().array(),
-  flags: z.string().array().optional(),
-  hide_versions: z.boolean().optional(),
-  license: license.array().length(1).optional(),
-  docs: docs.array().length(1).optional(),
+  components: z.string().array().min(1, 'At least one component is required.'),
+  // optionals
+  flags: z.string().array().default([]),
+  hide_versions: z.boolean().default(false),
+  license: license
+    .array()
+    .length(1)
+    .default([{ type: null, url: null }]),
+  docs: docs
+    .array()
+    .length(1)
+    .default([
+      {
+        process_docs: true,
+        readme_location: './README.md',
+      },
+    ]),
 })
 
 const schema = z.object({
