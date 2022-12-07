e003e81ca35f398f99a80540e563f4473f455d2c
Bryce Kalow <bkalow@hashicorp.com>
2022-12-07 09:40:01 -0600

    add support for multiple reporters

--- packages/content-conformance/src/runner.ts
+++ packages/content-conformance/src/runner.ts
@@ -1,4 +1,3 @@
-import report from 'vfile-reporter'
 import path from 'path'
 
 import { ContentConformanceConfig, loadConfig } from './config.js'
@@ -6,10 +5,13 @@ import { ContentConformanceEngine } from './engine.js'
 import { loadRules } from './rules.js'
 import type { LoadedConformanceRule } from './types.js'
 
+type Reporter = 'text' | 'json' | 'markdown'
+
 interface RunnerOptions {
   cwd?: string
   config?: string
   files?: string[]
+  reporter?: Reporter
 }
 
 /**
@@ -30,6 +32,7 @@ export class ContentConformanceRunner {
       cwd: opts?.cwd ?? process.cwd(),
       // normalize the passed in filepaths here to ensure consistent equality checks against found paths
       files: (opts?.files ?? []).map((filepath) => path.normalize(filepath)),
+      reporter: opts?.reporter ?? 'text',
     }
   }
 
@@ -58,7 +61,29 @@ export class ContentConformanceRunner {
    * TODO: support arbitrary reporters
    */
   async report() {
-    // @ts-expect-error -- need to sort out VFile types here
-    return report(this.engine?.files, { color: false, quiet: true })
+    if (!this.engine) {
+      throw new Error(
+        '[content-conformance] engine instance not found, did you call runner.run()?'
+      )
+    }
+
+    switch (this.opts.reporter) {
+      case 'markdown': {
+        // TODO: used for constructing a GitHub markdown comment
+        throw new Error('not implemented!')
+      }
+      case 'json': {
+        const report = (await import('vfile-reporter-json')).default
+
+        return report(this.engine.files, { quiet: true })
+      }
+      case 'text':
+      default: {
+        const report = (await import('vfile-reporter')).default
+
+        // @ts-expect-error -- need to sort out VFile types here
+        return report(this.engine.files, { color: false, quiet: true })
+      }
+    }
   }
 }
