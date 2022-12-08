bee5896da8df241193677324a99883d8e5d1ce0e
Bryce Kalow <bkalow@hashicorp.com>
2022-12-08 11:40:35 -0600

    feat(content-conformance): add ensure-valid-nav-data rule (#128)

--- packages/content-conformance/src/test/utils.ts
+++ packages/content-conformance/src/test/utils.ts
@@ -1,5 +1,9 @@
 import path from 'path'
 import url from 'url'
+import { VFileCompatible } from 'vfile'
+import { ContentFile } from '../content-file.js'
+import { DataFile } from '../data-file.js'
+import { ConformanceRuleBase, ConformanceRuleContext } from '../types.js'
 
 const currentFilePath = url.fileURLToPath(new URL(import.meta.url))
 
@@ -11,4 +15,109 @@ export function getFixturePath(fixtureName: string) {
     '__fixtures__',
     fixtureName
   )
+}
+
+function makeTestFileAndContext(rule: ConformanceRuleBase, fixture: string) {
+  let file
+  const context: ConformanceRuleContext = {
+    dataFiles: [],
+    contentFiles: [],
+    report: () => {
+      void 0
+    },
+  }
+
+  switch (rule.type) {
+    case 'data': {
+      file = new DataFile(fixture)
+      context.dataFiles = [file]
+      break
+    }
+    default:
+    case 'content': {
+      file = new ContentFile(fixture)
+      context.contentFiles = [file]
+      break
+    }
+  }
+
+  context.report = (message, file, node) => {
+    if (file) {
+      file.message(message, node, rule.id)
+    }
+  }
+
+  return [file, context] as const
+}
+
+/**
+ * A test helper to write assertions against a conformance rule.
+ *
+ * Usage:
+ *
+ * ```js
+ * testRule(myRule, [{ fixture: JSON.stringify([]), messages: ['No empty array'] }])
+ * ```
+ */
+export function testRule(
+  rule: ConformanceRuleBase,
+  testCases: { fixture: VFileCompatible; messages: (string | RegExp)[] }[]
+) {
+  function test(testCase: any) {
+    const [file, context] = makeTestFileAndContext(rule, testCase.fixture)
+
+    switch (rule.type) {
+      case 'data': {
+        rule.executor?.dataFile?.(
+          file as DataFile,
+          context as ConformanceRuleContext
+        )
+        break
+      }
+      case 'content': {
+        rule.executor?.contentFile?.(
+          file as ContentFile,
+          context as ConformanceRuleContext
+        )
+      }
+    }
+
+    // Handle the case where messages is empty, indicating we aren't expecting any messages to be reported.
+    if (testCase.messages.length === 0 && file.messages.length > 0) {
+      const err = new Error(`Expected no reported messages, but found:
+${file.messages.map(({ reason }) => `\t- ${reason}`).join('\n')}`)
+      err.stack = undefined
+
+      throw err
+    }
+
+    expect(() => {
+      testCase.messages.every((message: string | RegExp) => {
+        const match = file.messages.some((msg) => {
+          if (typeof message === 'string') {
+            return msg.reason === message
+          } else {
+            return msg.message.match(message)
+          }
+        })
+
+        if (!match) {
+          const err = new Error(
+            `\nexpected reported messages:
+${file.messages.map(({ reason }) => `\t- ${reason}`).join('\n')}
+to contain message matching:
+  ${message}`
+          )
+
+          err.stack = undefined
+
+          throw err
+        }
+      })
+    }).not.toThrow()
+  }
+
+  for (const testCase of testCases) {
+    test(testCase)
+  }
 }
