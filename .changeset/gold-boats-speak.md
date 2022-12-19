a7a9eec6e6afbf5535ce33ba643cd0dcd09ceea3
Bryce Kalow <bkalow@hashicorp.com>
2022-12-19 16:51:58 -0600

    feat(content-conformance): add GH comment reporter (#134)
    Co-authored-by: Kevin Wang <kwangsan@gmail.com>

---
+++ .changeset/gold-boats-speak.md
@@ -0,0 +1,5 @@
+---
+'@hashicorp/platform-content-conformance': patch
+---
+
+Add the `markdownReporter` to build markdown for a PR comment.
---
+++ packages/content-conformance/src/reporters/markdown-reporter.ts
@@ -0,0 +1,61 @@
+import { stringifyPosition } from 'unist-util-stringify-position'
+import { VFile } from 'vfile'
+import { statistics, VFileMessage } from 'vfile-statistics'
+
+export function markdownReporter(files: VFile[]) {
+  // @ts-expect-error - something is wrong with the VFile type used in vfile-statistics
+  const stats = statistics(files)
+
+  const lines = ['### 📄 Content Checks', '']
+
+  // commit & timestamp
+
+  // Make the timestamp generic so we can snapshot the output
+  const updatedTimestamp =
+    process.env.NODE_ENV === 'test' ? 'Date' : new Date().toUTCString()
+  lines.push(`Updated: ${updatedTimestamp}`, '')
+
+  // <details> header
+  lines.push(`<details><summary>Found ${stats.fatal} error(s)</summary>`, '')
+
+  // Each file has section led by a heading, and a table with each violation
+  files.forEach((file: VFile) => {
+    // Heading and Table heading
+    lines.push(
+      `#### \`${file.path}\``,
+      '',
+      '| Position | Description | Rule |',
+      '|---|---|---|'
+    )
+
+    // Table row for each message
+    file.messages.forEach((message: VFileMessage) => {
+      const columns = []
+
+      if (message.position) {
+        columns.push(`\`${stringifyPosition(message.position)}\``)
+      } else {
+        columns.push('')
+      }
+
+      columns.push(message.reason)
+
+      // TODO: link to descriptive page for the rule
+      columns.push(`\`${message.ruleId}\``)
+
+      lines.push(`|${columns.join('|')}|`, '')
+    })
+  })
+
+  // Footnote
+  // TODO: link to a README
+  //  lines.push(
+  //    '_Looking for more information? Check out the [content checks README](#TODO)_',
+  //    ''
+  //  )
+
+  // Details footer
+  lines.push('</details>')
+
+  return lines.join('\n')
+}
