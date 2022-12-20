f216c7cfcddb303d21f90c73261c210db5c2bd09
Bryce Kalow <bkalow@hashicorp.com>
2022-12-20 10:18:52 -0600

    use mdast-util-to-string

--- packages/content-conformance/src/rules/no-link-in-heading.js
+++ packages/content-conformance/src/rules/no-link-in-heading.js
@@ -1,17 +1,5 @@
 import { visit } from 'unist-util-visit'
-
-/**
- * Visit each node in a subtree to build a string representation
- */
-function getStringValue(node) {
-  let string = ''
-  visit(node, (child) => {
-    if ('value' in child) {
-      string += child.value
-    }
-  })
-  return string
-}
+import { toString } from 'mdast-util-to-string'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 /** @type {import('../types.js').ConformanceRuleBase} */
@@ -23,11 +11,11 @@ export default {
   executor: {
     async contentFile(file, context) {
       file.visit(['heading'], (node) => {
-        const headingStringValue = getStringValue(node)
+        const headingStringValue = toString(node)
 
         // visit any link nodes in the heading subtree
         visit(node, ['link'], (linkNode) => {
-          const linkStringValue = getStringValue(linkNode)
+          const linkStringValue = toString(linkNode)
 
           context.report(
             `Heading with text "${headingStringValue}" contains a link with text "${linkStringValue}". Remove the nested link, as links in headings can cause issues with anchor link generation and accessibility.`,
