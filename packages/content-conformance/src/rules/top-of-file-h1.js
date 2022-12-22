a9face69e7920b3942e1d214449b338513f26341
Nick Ayres <nickolas.ayres@hashicorp.com>
2022-12-21 20:28:27 -0700

    add partials ignore condition

--- packages/content-conformance/src/rules/top-of-file-h1.js
+++ packages/content-conformance/src/rules/top-of-file-h1.js
@@ -6,6 +6,9 @@ export default {
   description: 'Each content file must contain an H1 the top.',
   executor: {
     async contentFile(file, context) {
+      // Ignore partials as they do not represent the entire content file
+      if (file.isPartial) return false
+
       file.visit(['root'], (node) => {
         const firstNode = node.children[0]
         const reportMessage = `Expected file to start with a level 1 heading. A level 1 heading is important for accessibility and layout consistency. Add a level 1 heading at the top of the document.`
