13c2d9988bd8e336da5813910611dc60bcf13aae
Dylan Staley <88163+dstaley@users.noreply.github.com>
2023-03-08 10:55:24 -0800

    ci: correctly access rivet options

--- packages/cms/index.test.js
+++ packages/cms/index.test.js
@@ -3,7 +3,7 @@ const config = require('./config')
 
 test('configuration is passed into rivet correctly', () => {
   expect(rivet.client.url).toBe(config.url)
-  expect(rivet.client.options.headers['Authorization']).toBe(
+  expect(rivet.client.requestConfig.headers['Authorization']).toBe(
     config.headers['Authorization']
   )
 })
